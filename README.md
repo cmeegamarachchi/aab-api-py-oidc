48a5069a3671bd9e15910ab81eb82db17f892510
Chintana Meegamarachchi <chintana@appsolve.com.au>
2025-02-08 13:26:11 +0000

    Adds auth

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 # aaf-api-py
 appsolve application blocks fast-api starter kit  
 
-`aab-api-py` is a Python, Fast-API based api starter kit. Its build around best practices such as router based feature moudles.  
+`aab-api-py-oicc` is a Python, Fast-API based api starter kit with open-id-connect authentication. Its build around best practices such as router based feature moudles.  
 
 Some of the highlevel features  
 1. Feature modules. All features are hosted in `features` folder and common code is hosted in `common` folder
 
 ### To start
-Install the dependencies  
+
+Add required environment. `.env` at root level is supported. Following environment variables are required  
+  * `OIDC_APP_CLIENT_ID`: App client id
+  * `OIDC_JWKS_URL`: JWT keys url
+    * For Azure: `https://login.microsoftonline.com/[TENENT_ID]/discovery/v2.0/keys`
+    * For cognito: `https://cognito-idp.{COGNITO_REGION}.amazonaws.com/{COGNITO_USERPOOL_ID}/.well-known/jwks.json`
+  * `OIDC_ISSUER`:
+    * For Azure: `https://login.microsoftonline.com/ae5a8cb0-673c-43ba-826c-c2d6c87f2d70/v2.0`
+    * For cognito: `https://cognito-idp.{COGNITO_REGION}.amazonaws.com/{COGNITO_USERPOOL_ID}` 
+
+Install the dependencies and execute 
 ```bash
 cd api
 pip install -r requirements.txt
--- api/features/customers/routes.py
+++ api/features/customers/routes.py
@@ -2,9 +2,11 @@ import json
 import aiofiles
 import os
 from typing import List
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Depends
 from api.common.utils import read_from_json
 from api.features.customers.schema import Customer
+
+from api.common.auth import get_current_user
 
 customer_router = APIRouter()
 
@@ -19,12 +21,12 @@ async def write_customers_to_json(data: List[Customer]):
         await f.write(json.dumps([c.dict() for c in data], indent=4))
 
 @customer_router.get("/", response_model=List[Customer])
-async def get_all_customers():
+async def get_all_customers(user: dict = Depends(get_current_user)):
     customers = await read_customers_from_json()
     return customers
 
 @customer_router.get("/{customer_id}", response_model=Customer, status_code=status.HTTP_200_OK)
-async def get_customer(customer_id: str):
+async def get_customer(customer_id: str, user: dict = Depends(get_current_user)):
     customers = await read_customers_from_json()
     customer = next((c for c in customers if c.id == customer_id), None)
     if customer:
@@ -32,14 +34,14 @@ async def get_customer(customer_id: str):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found")
 
 @customer_router.post("/", response_model=Customer, status_code=status.HTTP_201_CREATED)
-async def create_customer(customer: Customer):
+async def create_customer(customer: Customer, user: dict = Depends(get_current_user)):
     customers = await read_customers_from_json()
     customers.append(customer)
     await write_customers_to_json(customers)
     return customer
 
 @customer_router.put("/{customer_id}", response_model=Customer, status_code=status.HTTP_200_OK)
-async def update_customer(customer_id: str, customer: Customer):
+async def update_customer(customer_id: str, customer: Customer, user: dict = Depends(get_current_user)):
     customers = await read_customers_from_json()
     customer_index = next((i for i, c in enumerate(customers) if c.id == customer_id), None)
     if customer_index is not None:
@@ -49,7 +51,7 @@ async def update_customer(customer_id: str, customer: Customer):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found")
 
 @customer_router.delete("/{customer_id}", status_code=status.HTTP_200_OK)
-async def delete_customer(customer_id: str):
+async def delete_customer(customer_id: str, user: dict = Depends(get_current_user)):
     customers = await read_customers_from_json()
     customer_index = next((i for i, c in enumerate(customers) if c.id == customer_id), None)
     if customer_index is not None:
